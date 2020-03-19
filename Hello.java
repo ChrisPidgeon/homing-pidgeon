6f11e96b5903be7198d647328a85985ad7966055
ChrisPidgeon <chris.j.pidgeon.98@gmail.com>
2020-03-19 14:20:29 +1300

    Testing the workflow pt. 2!

--- Hello.java
+++ Hello.java
@@ -6,5 +6,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("This is a test!");
+        System.out.println("This is a second test!");
     }
 }
